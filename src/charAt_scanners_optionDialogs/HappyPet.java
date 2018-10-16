c962e76570731b622c04dc2f02b1e089dbecefcf
601115 <601115@0338S-S-R0B6ZHK.sdcs.local>
2018-10-16 14:22:23 -0700

    Happy pet incomplete

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -5,20 +5,28 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+			String input = JOptionPane.showInputDialog("What kind of pet do you want?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Happiness", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "cuddle", "feed", "walk" }, null);
+				System.out.println(task);
+					
+				
 			// 5. Use user input to call the appropriate method created in step 4.
-
+				if(task==0) {
+					cuddlePet();
+				}else if(task==1) {
+					FeedPet();
+				}else if(task==2) {
+					walkPet();
+				}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
@@ -27,4 +35,18 @@ public class HappyPet {
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+static void cuddlePet() {
+	JOptionPane.showMessageDialog(null, "Thanks!");
+	happinessLevel++;
 }
+static void FeedPet() {
+	JOptionPane.showMessageDialog(null,"Your pet is full.");
+	happinessLevel++;
+
+}
+static void walkPet() {
+	JOptionPane.showMessageDialog(null, "Your pet is happy");
+	happinessLevel++;
+}
+}
+ 
