e5e5e1f082242572d87cab32bae7c5991639e0e0
601115 <601115@0338S-S-R0B6ZHK.sdcs.local>
2018-10-23 14:24:53 -0700

    Happy Pet

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -14,22 +14,26 @@ public class HappyPet {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
+			for (int i = 0; i < 10; i++) {
 			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Happiness", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "cuddle", "feed", "walk" }, null);
 				System.out.println(task);
 					
 				
 			// 5. Use user input to call the appropriate method created in step 4.
-				if(task==0) {
+				 if(task==0) {
 					cuddlePet();
 				}else if(task==1) {
 					FeedPet();
 				}else if(task==2) {
 					walkPet();
 				}
+			}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+				if(happinessLevel<2) {
+					JOptionPane.showMessageDialog(null,"Wow! You really love your pet.");
+				}
 	}
 
 	// 4. Create methods to handle each of your user selections.
