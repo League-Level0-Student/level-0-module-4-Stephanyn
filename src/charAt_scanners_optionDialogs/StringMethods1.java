7e358846ce211854e9f95d120977faf86ff448a0
601115 <601115@0338S-S-R0B6ZHK.sdcs.local>
2018-11-06 14:22:26 -0800

    string method and change calculator(incomplete)

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -8,7 +8,7 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-String Pi = "3.1415926535897932384";
+String Pi = "3.14159265";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
 		System.out.println(Pi.charAt(3));
@@ -20,13 +20,19 @@ String Pi = "3.1415926535897932384";
 		for (int i = 0; i < Pi.length(); i++) {
 			System.out.println(Pi.charAt(i));
 		}
+			Pi.charAt(9);
+			for (int i = 0; i <Pi.length(); i++) {
+			if(Pi.charAt(i)=='9') { 
+			System.out.println("9 is at index 7");
+				}
+			}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+			
 	}
 }
 
