0f1be8e5e92c6cb01245104a3a9093dd9d7e4adb
601115 <601115@0338S-S-R0B6ZHK.sdcs.local>
2018-11-13 14:27:58 -0800

    change calculator and sleepy head incomplete

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,22 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-	String m =	JOptionPane.showInputDialog("How many nickles do you have?");
+	String input =	JOptionPane.showInputDialog("How many nickles do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+		int userN = Integer.parseInt(input);
 		// Ask the user how many dimes they have, and convert their answer
-
+         String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+         int userD = Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
+         String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+         int userQ = Integer.parseInt(quarters);
 		// Calculate how much money the user has and save it in a double variable 
-
+         double x = 0.0;
+        x = x +(userN *.05);
+        x = x +(userD * .10);
+         x = x + (userQ *.25);
 		// Tell the user how much money they have
-
+         JOptionPane.showMessageDialog(null,"Your total value is"+x);
 	}
 }
 
--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -18,8 +18,8 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
-
+        JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+        	
 
         /*
          * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
