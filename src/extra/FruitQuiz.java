8a866df2375df3289f250ea8daafa2104d4a2d0b
601115 <601115@0338S-S-R0B6ZHK.sdcs.local>
2018-12-04 14:24:53 -0800

    Fruit quiz

--- src/extra/FruitQuiz.java
+++ src/extra/FruitQuiz.java
@@ -15,6 +15,8 @@ public class FruitQuiz extends KeyAdapter {
 
 	void makeQuestions() {
 		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
+		question2 = new JLabel("<html>What is the name of the emperor of the Yuan dynasty?<br>A:Genghis Khan <br>B:Khubilai Khan <br>C: Shi Huangdi </html>");
+		
 		// 11. Make another question called "question2".  Use question1 above as a guide.
 	}
 
@@ -22,19 +24,26 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+			System.out.print(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+			int A = 65;
+			int B = 66;
+			int C = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
-			
+			if(question)
 			// 13. check if it is right or wrong like you did for question1
 		
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
-			
+			if(question1.equals("C")) {
+				correct();
+				nextQuestion();
+			}else if(question1.equals("A")){
+				incorrect();
+			}else if(question1.equals("B"));
 				// 4. Call the correct() method
 				
 				// 7. Use the nextQuestion() method to go to question2
@@ -53,13 +62,13 @@ public class FruitQuiz extends KeyAdapter {
 		// the 'extra' package. It must be a .wav file. 
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+			playSound("correct.wav");
 
 	}
 
 	private void incorrect() {
 		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
-
+		playWrong("wrong.wav");
 	}
 
 	private void nextQuestion(JLabel newQuestion) {
