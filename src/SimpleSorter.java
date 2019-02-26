fb5ea657f2e6d00f0e891ce273ef632507abbc4c
601115 <601115@0338S-S-R0B6ZHK.sdcs.local>
2019-02-26 14:22:09 -0800

    simple sorter (incomplete)

--- src/SimpleSorter.java
+++ src/SimpleSorter.java
@@ -4,18 +4,39 @@ public class SimpleSorter {
 	public static void main(String[] args) {
 		String userNums= JOptionPane.showInputDialog("Give me a random number.");
 		String usernum2=JOptionPane.showInputDialog("Give me another number.");
-		int input=Integer.parseInt(userNums);
-		int input2=Integer.parseInt(usernum2);
-			if(input>input2) {
-				System.out.println(input);
-				System.out.println(input2);
-			}else if(input<input2) {
-				System.out.println(input2);
-				System.out.println(input);
+		String usernum3= JOptionPane.showInputDialog("Give me one last number.");
+		int a=Integer.parseInt(userNums);
+		int b=Integer.parseInt(usernum2);
+		int c= Integer.parseInt(usernum3);
+			if(a>b) {
+				int temp = 0;
+				temp=a;
+				a=b;
+				b=temp;
+			}else if (b>a) {
+				int temp = 0;
+				temp=b;
+				b=a;
+				a=temp;
+			}else if (c>a){
+				int temp = 0;
+				temp=c;
+				c=a;
+				a=temp;
+			}else if (c>b){
+				int temp = 0;
+				temp=c;
+				c=b;
+				b=temp;
+			}else if (a>c){
+				int temp = 0;
+				temp=a;
+				a=c;
+				c=temp;
 			}
 		
 		
-		
+		System.out.println(a+" "+b+" "+c);
 		
 
 		
